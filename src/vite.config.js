e38532caaf72cb578d7bf2ee6cee019b7f1c8ab0
mittimithai <maneeshkyadav@gmail.com>
2025-02-25 11:59:44 -0800

    added prod related config

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,9 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
+export default defineConfig(({ mode }) {
+    plugins: [react()],
+    server: {
     port: 5173
-  }
+    },
+    // Base configuration
+    base: mode === 'production' ? '/var/www/pubnostr/' : '/',
+    // Other config options
 })
