a2139b56cb944d1a434723d4399fa6d409caaac7
mittimithai <maneeshkyadav@gmail.com>
2025-02-25 12:47:19 -0800

    fix config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig( ({ mode }) => {
     return{
-	base: mode === 'production' ? '/var/www/pubnostr.com/' : '/',
+	base: mode === 'production' ? '/' : '/',
 	plugins: [react()],
 	server: {
 	    port: 5173
